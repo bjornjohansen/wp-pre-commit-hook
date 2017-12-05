64e921d5866c05ae458abb0f56cc69e26e27768a
Bjørn Johansen <post@bjornjohansen.no>
2017-12-05 16:24:46 +0000

    Create Installer.php

---
+++ src/Installer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BJWPPreCommitHook;
+
+class Installer
+{
+    public static function postInstall()
+    {
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            system('cmd /c vendor\bjornjohansen\wp-pre-commit-hook\src\setup.bat');
+        }
+        else {
+            system('sh vendor/bjornjohansen/wp-pre-commit-hook/src/setup.sh');
+        }
+    }
+}
