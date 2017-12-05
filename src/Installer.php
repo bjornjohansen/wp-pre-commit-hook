8f354d27a19cee58e2baa65f72f1a7ab2460f1a7
Bjørn Johansen <post@bjornjohansen.no>
2017-12-05 16:32:32 +0000

    Skip Windows compat for now

--- src/Installer.php
+++ src/Installer.php
@@ -7,7 +7,7 @@ class Installer
     public static function postInstall()
     {
         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            system('cmd /c vendor\bjornjohansen\wp-pre-commit-hook\src\setup.bat');
+            //system('cmd /c vendor\bjornjohansen\wp-pre-commit-hook\src\setup.bat');
         }
         else {
             system('sh vendor/bjornjohansen/wp-pre-commit-hook/src/setup.sh');
